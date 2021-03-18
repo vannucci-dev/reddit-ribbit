d6d6fdd3a40c13ad9dd9b0370b33dd32ed845408
Lorenzo Vannucci <info@vannucci-dev.com>
2021-03-18 12:41:06 +0000

    create structure and style searchBar

---
+++ src/features/searchBar/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./searchBar.css";
+
+const SearchBar = () => {
+  const [value, setValue] = useState("");
+
+  const handleKey = (e) => {
+    if (e.key === "Enter") {
+      setValue("");
+    }
+  };
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <input
+      className="searchBar"
+      type="text"
+      value={value}
+      placeholder="ribbit ribbit..."
+      onChange={handleChange}
+      onKeyDown={handleKey}
+    ></input>
+  );
+};
+
+export default SearchBar;
