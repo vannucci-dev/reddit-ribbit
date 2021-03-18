defb8f5b9a601fe769acb1c63c17e5a19cb031c8
Lorenzo Vannucci <info@vannucci-dev.com>
2021-03-18 18:48:28 +0000

    Structure and style subscribedList

--- src/features/subscribed/SubscribedList.js
+++ src/features/subscribed/SubscribedList.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "./subscribedList.css";
 
 const SubscribedList = () => {
-  return <div>SubscribedList</div>;
+  return (
+    <div className="subcontainer">
+      <div className="subs-list">
+        <div className="sub-box">
+          <div className="sub-img"></div>
+          <a href="#">/subreddit</a>
+        </div>
+        <div className="sub-box">
+          <div className="sub-img"></div>
+          <a href="#">/subreddit</a>
+        </div>
+        <div className="sub-box">
+          <div className="sub-img"></div>
+          <a href="#">/subreddit</a>
+        </div>
+        <div className="sub-box">
+          <div className="sub-img"></div>
+          <a href="#">/subreddit</a>
+        </div>
+      </div>
+      <button>+</button>
+    </div>
+  );
 };
 
 export default SubscribedList;
